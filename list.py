b664b21cf1c85fda6a185bc659171d23df6fd746
frankrogalski <frank.rogalski@hansa-flex.com>
2021-08-09 20:05:07 +0200

    first commit

---
+++ list.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.7,de;q=0.3',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Referer': 'https://www.netto-online.de/vereinsspende/projekte.html?postcode=28870&radius=3000',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-GPC': '1',
+    'TE': 'trailers',
+}
+
+vereine = []
+for i in range(160):
+    params = (
+        ('type', 'load_more'),
+        ('my_paged', str(i)),
+        ('postcode', '28870'),
+        ('radius', '30000'),
+        ('load_more_text', 'Mehr anzeigen'),
+        ('ajax', '1'),
+    )
+
+    response = requests.get('https://www.netto-online.de/vereinsspende/', headers=headers, params=params)#, cookies=cookies)
+
+    bs = BeautifulSoup(response.content, features='html.parser')
+    for card in bs.find_all('div', {'class': 'card'}):
+        title = card.find_next('h3', {'class': 'card-title'}).text
+        location = card.find_next('span', {'class': 'card-location'}).text
+        likes = int(card.find_next('span', {'class': 'count-box'}).text[1:].replace(".", ""))
+        vereine.append((title, likes, location))
+
+vereine = sorted(set(vereine), key=lambda x: x[1], reverse=True)
+with open('/Users/frankrogalski/Privat/Python/2021-08-09 sport.py/jo.txt', 'w') as file:
+    for verein in vereine:
+        file.write(str(verein) + '\n')
+
+print(len(vereine))
---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.7,de;q=0.3',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Origin': 'https://www.netto-online.de',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.netto-online.de/vereinsspende/vereinsspende.html',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-GPC': '1',
+    'TE': 'trailers',
+}
+
+data = {
+    'action': 'wp_ulike_process',
+    'id': '923',
+    'nonce': '1c9db1577e',
+    'type': 'post',
+    'template': 'wpulike-default',
+    'displayLikers': '0',
+    'likersTemplate': 'popover'
+}
+
+for i in range(10000):
+    requests.post('https://www.netto-online.de/vereinsspende/wp-admin/admin-ajax.php', headers=headers, data=data)
+    if (i + 1) % 100 == 0:
+        print(f'{i + 1} requests done')
